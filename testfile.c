83cbb2b78a3e984d6e3d2d7a0cc0dc713112d38d
mirza348 <99548639+mirza348@users.noreply.github.com>
2022-02-19 09:06:40 -0500

    Update testfile.c

--- testfile.c
+++ testfile.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
+//something for randomly generated input points use srand orbrand
+
+
+int main() {
+
+// create a queue  for the points
+
+
+//horizontal line y=mx +c
+double y = //random number
+double m = //random num
+
+
+
+------------------------------------------
+//start of circle
+//radius circle
+double r = 
+
+// intersecting circle code
+
+//
+
+
+}
